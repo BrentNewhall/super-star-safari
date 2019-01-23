6231870c054b220867636b8ea50ad4f7a3c9fe2f
Brent Newhall <brent.newhall@capitalone.com>
2019-01-23 11:53:14 -0500

    Added sidebar

--- src/App.js
+++ src/App.js
@@ -10,6 +10,16 @@ class App extends Component {
         <header className="App-header">
           Super Star Safari
         </header>
+        <div className="sidebar">
+          Condition: GREEN<br />
+          Life Support: GREEN<br />
+          Energy: 5000<br />
+          Torpedoes: 10<br />
+          Shields: UP, 100%<br />
+          Enemies left: 20<br />
+          Date: 2300<br />
+          Time Left: 20<br />
+        </div>
         {ship}
       </div>
     );
